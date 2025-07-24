e38a7858bd2daedd118f74d72f489f6d91d4bae6
Axel Pérez <axeleliasperezrodriguez2@gmail.com>
2025-07-24 16:24:31 -0600

    Para desplegar

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,14 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
+      transform : true,
+      transformOptions : {
+        enableImplicitConversion : true
+      }
     })
   );
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT!);
 }
 bootstrap();
+
+
